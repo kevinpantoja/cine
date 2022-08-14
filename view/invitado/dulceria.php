2377c9ef1f35d8a9ffd469d2ee7d75c46f1b8c7d
malthox <squintanillaj@unmsm.edu.pe>
2022-08-14 16:09:20 -0500

    confiteria

--- view/invitado/dulceria.php
+++ view/invitado/dulceria.php
@@ -9,61 +9,163 @@
 <body>
     <h1 class="titulo">COMPRA TUS BEBIDAS Y ALIMENTOS</h1>
     <section class="contenedor_confiteria">
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/palomita.jpg">
-               
+        <div class="marco">
+            
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/palomita.jpg">
+                
+                </div>
+                
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Pop Corn</h2>
+                    </div>
+                    <p class="confiteria_info">canchita salada</p>
+                    <p class="confiteria_tamano">tamaño</p>
+                    <select class="tamano">
+                        <option selected>tamaño</option>
+                        <option value="1">Pequeño</option>
+                        <option value="2">Mediano</option>
+                        <option value="3">Grande</option>
+                    </select>
+                    <p class="confiteria_precio_tamano">S/. 15.00</p>
+                </div>
+                
             </div>
-            <h2 class="confiteria_titulo">Pop Corn canchita salada</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/palomitadulce.jpg">
-               
+        
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/palomitadulce.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Pop Corn</h2>
+                    </div>
+                    <p class="confiteria_info">canchita dulce</p>
+                    <p class="confiteria_tamano">tamaño</p>
+                    <select class="tamano">
+                        <option selected>tamaño</option>
+                        <option value="1">Pequeño</option>
+                        <option value="2">Mediano</option>
+                        <option value="3">Grande</option>
+                    </select>
+                    <p class="confiteria_precio_tamano">S/. 15.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">Pop Corn canchita dulce</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/gaseosa.jpg">
-               
+
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/gaseosa.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Gaseosa</h2>
+                    </div>
+                    <p class="confiteria_info">Inka cola/Coca cola (21 oz)</p>
+                    <p class="confiteria_tamano">tamaño</p>
+                    <select class="tamano">
+                        <option selected>tamaño</option>
+                        <option value="1">Pequeño</option>
+                        <option value="2">Mediano</option>
+                        <option value="3">Grande</option>
+                    </select>
+                    <p class="confiteria_precio_tamano">S/. 15.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">Gaseosa:Inka cola/ Coca cola</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/nachos.jpg">
-               
+
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/nachos.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">nachos</h2>
+                    </div>
+                    <p class="confiteria_info">nachos peruanos + cremas</p>
+                    <p class="confiteria_precio">S/. 12.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">nachos peruanos + cremas</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/combo1.jpg">
-               
+
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/combo1.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Combo 1</h2>
+                    </div>
+                    <p class="confiteria_info">canchita salada + gaseosa 21 oz</p>
+                    <p class="confiteria_precio">S/. 28.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">combo 1: canchita salada + gaseosa 21 Oz</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/combo2.jpg">
-               
+
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/combo2.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Combo 2</h2>
+                    </div>
+                    <p class="confiteria_info">canchita salda + gaseosa 21 oz + hotdog</p>
+                    <p class="confiteria_precio">S/. 35.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">combo 2: canchita salda + gaseosa 21 oz + hotdog</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/combo3.jpg">
-               
+
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/combo3.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Combo 3</h2>
+                    </div>
+                    <p class="confiteria_info">Pop corn salada + pop corn dulce gigante</p>
+                    <p class="confiteria_precio">S/. 39.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">combo 3: Pop corn salada + pop corn dulce gigante</h2>
         </div>
-        <div class="confiteria_individual">
-            <div class="contenedor_imagen">
-                <img class="confiteria_imagen" src="public/img/photos/hogdog.jpg">
-               
+
+        <div class="marco">
+            <div class="confiteria_individual">
+                <div class="contenedor_imagen">
+                    <img class="confiteria_imagen" src="public/img/photos/hogdog.jpg">
+                
+                </div>
+
+                <div class="marco_nombre_producto">
+                    <div class="marco_titulo_producto">
+                        <h2 class="confiteria_titulo">Hot Dog</h2>
+                    </div>
+                    <p class="confiteria_info">Pan + Hot Dog jumbo</p>
+                    <p class="confiteria_precio">S/. 12.00</p>
+                </div>
             </div>
-            <h2 class="confiteria_titulo">Hot Dog: Pan + Hot Dog jumbo</h2>
         </div>
         
     </section>
