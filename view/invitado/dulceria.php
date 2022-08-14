689f56abd482741fedf82d846a9df664ac6da743
malthox <squintanillaj@unmsm.edu.pe>
2022-08-14 11:29:59 -0500

    ezZzZzZ

--- view/invitado/dulceria.php
+++ view/invitado/dulceria.php
@@ -1,1 +1,74 @@
-<h1>esta es la dulceria</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="titulo">COMPRA TUS BEBIDAS Y ALIMENTOS</h1>
+    <section class="contenedor_confiteria">
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/palomita.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">Pop Corn canchita salada</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/palomitadulce.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">Pop Corn canchita dulce</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/gaseosa.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">Gaseosa:Inka cola/ Coca cola</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/nachos.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">nachos peruanos + cremas</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/combo1.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">combo 1: canchita salada + gaseosa 21 Oz</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/combo2.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">combo 2: canchita salda + gaseosa 21 oz + hotdog</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/combo3.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">combo 3: Pop corn salada + pop corn dulce gigante</h2>
+        </div>
+        <div class="confiteria_individual">
+            <div class="contenedor_imagen">
+                <img class="confiteria_imagen" src="public/img/photos/hogdog.jpg">
+               
+            </div>
+            <h2 class="confiteria_titulo">Hot Dog: Pan + Hot Dog jumbo</h2>
+        </div>
+        
+    </section>
+
+</body>
+</html>
+
+
